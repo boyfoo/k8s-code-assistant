aa7b6ccbd491e5ca9f5df792fcd637956c2e44e1
zhengxin <zx358317301@qq.com>
2023-07-13 15:55:15 +0800

    first commit

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.18
 require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/sashabaranov/go-openai v1.13.0 // indirect
+	k8s.io/client-go v0.22.15 // indirect
 )
--- main.go
+++ main.go
@@ -1,55 +1,10 @@
 package main
 
-import "fmt"
-
-type Student struct {
-	Name string
-}
-
-func sa(arr []int) {
-	sum := 10
-	ret := make([][2]int, 0)
-	hash := make(map[int]int)
-	for i := 0; i < len(arr); i++ {
-		if v, ok := hash[sum-arr[i]]; ok {
-			ret = append(ret, [2]int{arr[v], arr[i]})
-		} else {
-			hash[arr[i]] = i
-		}
-	}
-	fmt.Println(ret)
-}
-
 func main() {
-	ss := []int{2, 4, 1, 6, 8, 4}
-	sa(ss)
+	//
+	//rest.InClusterConfig()
+	//
+	//rest.InClusterConfig()
+	//clientcmd.BuildConfigFromFlags("", "")
+	//clientSet, err := kubernetes.NewForConfig()
 }
-
-//func mainss() {
-//	err := godotenv.Load()
-//	if err != nil {
-//		log.Fatal("Error loading .env file")
-//	}
-//	s := os.Getenv("OPENAI_KEY")
-//
-//	client := openai.NewClient(s)
-//	resp, err := client.CreateChatCompletion(
-//		context.Background(),
-//		openai.ChatCompletionRequest{
-//			Model: openai.GPT3Dot5Turbo0613,
-//			Messages: []openai.ChatCompletionMessage{
-//				{
-//					Role:    openai.ChatMessageRoleUser,
-//					Content: "你好!",
-//				},
-//			},
-//		},
-//	)
-//
-//	if err != nil {
-//		fmt.Printf("ChatCompletion error: %v\n", err)
-//		return
-//	}
-//
-//	fmt.Println(resp.Choices[0].Message.Content)
-//}
