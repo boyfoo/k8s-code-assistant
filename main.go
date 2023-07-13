55c19b60fd254ae46672de0cf8406b83753736af
zhengxin <zx358317301@qq.com>
2023-07-13 16:36:24 +0800

    first commit

--- main.go
+++ main.go
@@ -1,12 +1,5 @@
 package main
 
-import "k8s.io/client-go/rest"
-
 func main() {
-	//
-	rest.InClusterConfig()
-	//
-	//rest.InClusterConfig()
-	//clientcmd.BuildConfigFromFlags("", "")
-	//clientSet, err := kubernetes.NewForConfig()
+
 }
